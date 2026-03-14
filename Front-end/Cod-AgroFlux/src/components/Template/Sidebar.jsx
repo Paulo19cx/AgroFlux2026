eba8e4aea96c949c6b95e442e9b2b65d560c60dc
Paulo Henrique <phcodonho@gmail.com>
2026-03-13 22:35:53 -0300

    Style: Tentei mudar a largura da sidebar, mas não funcionou

--- Front-end/Cod-AgroFlux/src/components/Template/Sidebar.jsx
+++ Front-end/Cod-AgroFlux/src/components/Template/Sidebar.jsx
@@ -19,42 +19,42 @@ function Sidebar() {
                     <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto ms-3">
                         <ul className="nav flex-column gap-3">
                             <li className="nav-item">
-                                <Link to="/" className="nav-link d-flex align-items-center fw-normal gap-3 active text-black" aria-current="page" href="#">
+                                <Link to="/" className="nav-link d-flex align-items-center fw-normal gap-3 active text-black" aria-current="page">
                                 <i className="fi fi-rr-home" aria-hidden="true"></i>Dashboard</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/clientes" className="nav-link d-flex align-items-center fw-normal gap-3 text-black" href="#">
+                                <Link to="/clientes" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
                                 <i className="fi fi-rs-users" aria-hidden="true"></i>Clientes</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/funcionarios" className="nav-link d-flex align-items-center fw-normal gap-3 text-black" href="#">
+                                <Link to="/funcionarios" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
                                 <i className="fi fi-rs-user-gear" aria-hidden="true"></i>Funcionários</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/produtos" className="nav-link d-flex align-items-center fw-normal gap-3 text-black" href="#">
+                                <Link to="/produtos" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
                                 <i className="fi fi-rr-box-open-full" aria-hidden="true"></i>Produtos</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/vendas" className="nav-link d-flex align-items-center fw-normal gap-3 text-black" href="#">
+                                <Link to="/vendas" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
                                 <i className="fi fi-rr-shopping-cart" aria-hidden="true"></i>Vendas</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/fornecedores" className="nav-link d-flex align-items-center fw-normal gap-3 text-black" href="#">
+                                <Link to="/fornecedores" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
                                 <i className="fi fi-rr-truck-side" aria-hidden="true"></i>Fornecedores</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/estoque" className="nav-link d-flex align-items-center fw-normal gap-3 text-black" href="#">
+                                <Link to="/estoque" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
                                 <i className="fi fi-rr-warehouse-alt" aria-hidden="true"></i>Estoque</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/relatorios" className="nav-link d-flex align-items-center fw-normal gap-3 text-black" href="#">
-                                <i className="fi fi-rr-ballot" aria-hidden="true"></i>Relátorios</Link>
+                                <Link to="/relatorios" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
+                                <i className="fi fi-rr-ballot" aria-hidden="true"></i>Relatórios</Link>
                             </li>
                         </ul>
                     </div>
