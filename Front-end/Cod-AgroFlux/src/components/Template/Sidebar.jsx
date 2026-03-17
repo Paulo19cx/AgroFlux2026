a592602889402bcb9b2a0c5e7671c00c8a8df90f
Paulo Henrique Codonho Floriano <phcodonho@gmail.com>
2026-03-17 15:38:05 -0300

    Style: Alinhanto o titulo da página cliente

--- Front-end/Cod-AgroFlux/src/components/Template/Sidebar.jsx
+++ Front-end/Cod-AgroFlux/src/components/Template/Sidebar.jsx
@@ -19,41 +19,41 @@ function Sidebar() {
                     <div className="sidebar pt-lg-3 ph-bg-sidebar-navbar">
                         <ul className="nav gap-3">
                             <li className="">
-                                <Link to="/" className="nav-link d-flex fw-normal ps-4 gap-3 text-black" aria-current="page">
+                                <Link to="/" className="nav-link d-flex fw-normal ph-padding-left gap-3 text-black" aria-current="page">
                                     <i className="fi fi-rr-home" aria-hidden="true"></i>Dashboard</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/clientes" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
+                                <Link to="/clientes" className="nav-link d-flex align-items-center fw-normal ph-padding-left gap-3 text-black">
                                     <i className="fi fi-rs-users" aria-hidden="true"></i>Clientes</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/funcionarios" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
+                                <Link to="/funcionarios" className="nav-link d-flex align-items-center fw-normal ph-padding-left gap-3 text-black">
                                     <i className="fi fi-rs-user-gear" aria-hidden="true"></i>Funcionários</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/produtos" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
+                                <Link to="/produtos" className="nav-link d-flex align-items-center fw-normal ph-padding-left gap-3 text-black">
                                     <i className="fi fi-rr-box-open-full" aria-hidden="true"></i>Produtos</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/vendas" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
+                                <Link to="/vendas" className="nav-link d-flex align-items-center fw-normal ph-padding-left gap-3 text-black">
                                     <i className="fi fi-rr-shopping-cart" aria-hidden="true"></i>Vendas</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/fornecedores" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
+                                <Link to="/fornecedores" className="nav-link d-flex align-items-center fw-normal ph-padding-left gap-3 text-black">
                                     <i className="fi fi-rr-truck-side" aria-hidden="true"></i>Fornecedores</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/estoque" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
+                                <Link to="/estoque" className="nav-link d-flex align-items-center fw-normal ph-padding-left gap-3 text-black">
                                     <i className="fi fi-rr-warehouse-alt" aria-hidden="true"></i>Estoque</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/relatorios" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
+                                <Link to="/relatorios" className="nav-link d-flex align-items-center fw-normal ph-padding-left gap-3 text-black">
                                     <i className="fi fi-rr-ballot" aria-hidden="true"></i>Relatórios</Link>
                             </li>
                         </ul>
