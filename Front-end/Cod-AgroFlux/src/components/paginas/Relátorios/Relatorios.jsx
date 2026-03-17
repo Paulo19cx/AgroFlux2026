cf7cf4da938826d5c3debffbf04104ce0c3939fc
Paulo Henrique Codonho Floriano <phcodonho@gmail.com>
2026-03-17 14:41:31 -0300

    Style: Novamente erro com a sidebar em questão ao espaçamento, mas ja foi resolvido

--- Front-end/Cod-AgroFlux/src/components/paginas/Relátorios/Relatorios.jsx
+++ Front-end/Cod-AgroFlux/src/components/paginas/Relátorios/Relatorios.jsx
@@ -1,15 +1,12 @@
 import Sidebar from "../../template/Sidebar";
 import Navbar from "../../template/Navbar";
-import SelecionarTema from "../../SelecionarTema";
-import AlternadorDeTema from "../../AlternadorDeTema";
 import MainHome from "./MainRelatorio";
 
 function Relatorio() {
     return (
         <>
             <title>Relatórios</title>
-            <AlternadorDeTema />
-            <SelecionarTema />
+            
             <Navbar />
             <div className="container-fluid">
                 <div className="row">
--- Front-end/Cod-AgroFlux/src/components/paginas/Vendas/Vendas.jsx
+++ Front-end/Cod-AgroFlux/src/components/paginas/Vendas/Vendas.jsx
@@ -1,15 +1,12 @@
 import Sidebar from "../../template/Sidebar";
 import Navbar from "../../template/Navbar";
-import SelecionarTema from "../../SelecionarTema";
-import AlternadorDeTema from "../../AlternadorDeTema";
 import MainVendas from "./MainVendas";
 
 function Vendas() {
     return (
         <>
             <title>Vendas</title>
-            <AlternadorDeTema />
-            <SelecionarTema />
+     
             <Navbar />
             <div className="container-fluid">
                 <div className="row">
