40ad1a29641daa6e24d205ac3d3105bcd39ed0bb
Paulo Henrique Codonho Floriano <phcodonho@gmail.com>
2026-03-17 15:08:34 -0300

    Style: Arrumei o import do icons e alinhei novamente

--- Front-end/Cod-AgroFlux/src/App.jsx
+++ Front-end/Cod-AgroFlux/src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/css/dashboard.css'
 import './App.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../node_modules/@flaticon/flaticon-uicons/css/all/all.css'
 //import './assets/js/dashboard'
 import Home from './components/paginas/Home/Home';
 import Produtos from './components/paginas/Produtos/Produtos'
--- Front-end/Cod-AgroFlux/src/components/Template/Sidebar.jsx
+++ Front-end/Cod-AgroFlux/src/components/Template/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 function Sidebar() {
     return (
         <>
-            <div className="sidebar col-lg-1 ph-altura-sidebar p-0 mt-0 ph-bg-sidebar-navbar overflow-x-hidden p-0 pt-lg-3 overflow-y-auto ms-3">
+            <div className="sidebar col-lg-1 ph-altura-sidebar p-0 mt-0 ph-bg-sidebar-navbar overflow-x-hidden p-0 pt-lg-3 overflow-y-auto">
                 <div className="offcanvas-md offcanvas-end bg-body-tertiary" tabIndex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel ">
                     <div className="offcanvas-header">
                         <h5 className="offcanvas-title" id="sidebarMenuLabel">AgroFlux</h5>
@@ -19,41 +19,41 @@ function Sidebar() {
                     <div className="sidebar pt-lg-3 ph-bg-sidebar-navbar">
                         <ul className="nav gap-3">
                             <li className="">
-                                <Link to="/" className="nav-link d-flex fw-normal gap-3 text-black" aria-current="page">
+                                <Link to="/" className="nav-link d-flex fw-normal ps-4 gap-3 text-black" aria-current="page">
                                     <i className="fi fi-rr-home" aria-hidden="true"></i>Dashboard</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/clientes" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
+                                <Link to="/clientes" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
                                     <i className="fi fi-rs-users" aria-hidden="true"></i>Clientes</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/funcionarios" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
+                                <Link to="/funcionarios" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
                                     <i className="fi fi-rs-user-gear" aria-hidden="true"></i>Funcionários</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/produtos" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
+                                <Link to="/produtos" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
                                     <i className="fi fi-rr-box-open-full" aria-hidden="true"></i>Produtos</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/vendas" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
+                                <Link to="/vendas" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
                                     <i className="fi fi-rr-shopping-cart" aria-hidden="true"></i>Vendas</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/fornecedores" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
+                                <Link to="/fornecedores" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
                                     <i className="fi fi-rr-truck-side" aria-hidden="true"></i>Fornecedores</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/estoque" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
+                                <Link to="/estoque" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
                                     <i className="fi fi-rr-warehouse-alt" aria-hidden="true"></i>Estoque</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/relatorios" className="nav-link d-flex align-items-center fw-normal gap-3 text-black">
+                                <Link to="/relatorios" className="nav-link d-flex align-items-center fw-normal ps-4 gap-3 text-black">
                                     <i className="fi fi-rr-ballot" aria-hidden="true"></i>Relatórios</Link>
                             </li>
                         </ul>
