fabb3e6cdf4eafc2da4d0ff72d3bac8ed50be910
Henrique Guimarães <henriqueguimaraes957@gmail.com>
2026-03-20 15:36:46 -0300

    Feat: Adicionando o arquivo do grafico na página.

---
+++ Front-end/Cod-AgroFlux/src/components/Template/Grafico.jsx
@@ -0,0 +1,58 @@
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+} from "recharts";
+
+
+function BarraColorida(props) {
+  const { x, y, width, height, payload } = props;
+
+  return (
+    <rect
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      rx={1}
+      ry={1}
+      fill={payload.cor}
+    />
+  );
+}
+
+
+function Grafico() {
+    const dadosNotas = [
+        { nome: "Jan", nota: 80000, cor: '#16a34a' },
+        { nome: "Fev", nota: 75000, cor: '#ef4444'},
+        { nome: "Mar", nota: 60000, cor: '#16a34a' },
+        { nome: "Abr", nota: 55000, cor: '#ef4444' },
+        { nome: "Mai", nota: 65000, cor: '#16a34a' },
+        { nome: "Jun", nota: 45000, cor: '#ef4444' }
+        
+    ];
+
+
+    return (
+        <>
+            <div className="row">
+                <h3 className="mt-3 mb-3">Vendas x Custos (Últimos 6 Meses)</h3>
+                <BarChart responsive width={`100%`} height={300} data={dadosNotas}>
+                    <CartesianGrid strokeDasharray="5 5" />
+                    <XAxis dataKey="nome" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="nota" shape={<BarraColorida />} />
+                </BarChart>
+            </div>
+
+        </>
+    );
+}
+export default Grafico;
--- Front-end/Cod-AgroFlux/src/components/paginas/Relátorios/MainRelatorio.jsx
+++ Front-end/Cod-AgroFlux/src/components/paginas/Relátorios/MainRelatorio.jsx
@@ -1,4 +1,5 @@
 import Card from "../../Card";
+import Grafico from "../../Template/Grafico";
 
 function MainRelatorio() {
 
@@ -9,49 +10,86 @@ function MainRelatorio() {
                     <h1 class="h2">Relatório e Informações Gerais</h1>
                 </div>
 
+                {/* Código das Estastísticas */}
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-3 hh-mb">
                         <div class="card">
                             <div class="card-body">
                                 <h6 class="card-title text-secondary">Total de Vendas</h6>
                                 <p class="h3 card-text text-success">R$ 336.000</p>
                                 <p class="h6 card-text text-secondary">508 Transações</p>
-                                <div className="verde-circulo float-end"><i class="fi fi-br-cart-shopping-fast fs-5"></i></div>
+                                <div className="hh-verde-circulo float-end"><i class="fi fi-br-cart-shopping-fast hh-icon text-success"></i></div>
                             </div>
                         </div>
                     </div>
                     <div class="col-sm-3">
                         <div class="card ">
                             <div class="card-body">
-                            <h6 class="card-title text-secondary">Lucro Total</h6>
+                                <h6 class="card-title text-secondary">Lucro Total</h6>
                                 <p class="h3 card-text text-success">R$ 130.000</p>
                                 <p class="h6 card-text text-success">Margem: 38.7%</p>
-                                <div className="verde-circulo float-end"></div>
+                                <div className="hh-verde-circulo float-end"><i class="fi fi-br-arrow-trend-up hh-icon text-success"></i></div>
                             </div>
                         </div>
                     </div>
-                     <div class="col-sm-3">
+                    <div class="col-sm-3">
                         <div class="card">
                             <div class="card-body">
-                            <h6 class="card-title text-secondary">Custos Totais</h6>
+                                <h6 class="card-title text-secondary">Custos Totais</h6>
                                 <p class="h3 card-text text-danger">R$ 206.000</p>
                                 <p class="h6 card-text text-secondary">61.3% do faturamento</p>
-                                <div className="vermelho-circulo float-end"></div>
+                                <div className="hh-vermelho-circulo float-end"><i class="fi fi-br-arrow-trend-down hh-icon text-danger"></i></div>
                             </div>
                         </div>
                     </div>
-                     <div class="col-sm-3">
+                    <div class="col-sm-3">
                         <div class="card">
                             <div class="card-body">
-                            <h6 class="card-title text-secondary">Ticket Médio</h6>
+                                <h6 class="card-title text-secondary">Ticket Médio</h6>
                                 <p class="h3 card-text text-primary">R$ 661.42</p>
                                 <p class="h6 card-text text-secondary">Por venda</p>
-                                <div className="azul-circulo float-end"></div>
+                                <div className="hh-azul-circulo float-end"><i class="fi fi-br-dollar hh-icon text-primary"></i></div>
                             </div>
                         </div>
                     </div>
-
                 </div>
+
+                {/* Código Estastísticas do Estoque */}
+                <div class="row">
+                    <div class="col-sm-4 d-flex justify-content-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6 class="card-title text-secondary">Valor em Estoque</h6>
+                                <p class="h3 card-text text-roxo">R$ 50.600</p>
+                                <p class="h6 card-text text-secondary">6 Produtos</p>
+                                <div className="hh-roxo-circulo float-end"><i class="fi fi-br-box-open hh-icon text-roxo"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card ">
+                            <div class="card-body">
+                                <h6 class="card-title text-secondary">Estoque Total</h6>
+                                <p class="h3 card-text text-primary">750</p>
+                                <p class="h6 card-text text-secondary">Unidades</p>
+                                <div className="hh-azul-circulo float-end"><i class="fi fi-br-arrow-trend-up hh-icon text-primary"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6 class="card-title text-secondary">Estoque Total</h6>
+                                <p class="h3 card-text text-laranja">2</p>
+                                <p class="h6 card-text text-secondary">Abaixo do Mínimo</p>
+                                <div className="hh-laranja-circulo float-end"><i class="fi fi-br-diamond-exclamation hh-icon text-laranja"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Grafico de Barra */}
+                <Grafico />
 
             </main>
         </>
