79d6e1b4a95820e63e4d6b4e0c1d4afe8be16619
beatriz dos anjos <beatrizdosanjos3004@gmail.com>
2026-03-24 12:08:13 -0300

    Feat:adição do gráfico na página home

--- Front-end/Cod-AgroFlux/src/components/Template/GraficoLinha.jsx
+++ Front-end/Cod-AgroFlux/src/components/Template/GraficoLinha.jsx
@@ -1,38 +1,30 @@
 import { LineChart, Pie, XAxis, PieChart, YAxis, Line, CartesianGrid, Bar } from "recharts";
-  
 
 
 
-function GraficoLiha() {
+
+function GraficoLinha() {
+
     const dadosNotas = [
-        { nome: "Jan", nota: 80000, cor: '#16a34a' },
-        { nome: "Fev", nota: 75000, cor: '#16a34a' },
-        { nome: "Mar", nota: 60000, cor: '#16a34a' },
-        { nome: "Abr", nota: 55000, cor: '#16a34a' },
-        { nome: "Mai", nota: 65000, cor: '#16a34a' },
-        { nome: "Jun", nota: 45000, cor: '#16a34a' }
-
-    ];
-
+        { nome: 'Jan', nota: 8},
+        { nome: 'Fev', nota: 6},
+        { nome: 'Mar', nota: 9},
+        { nome: 'Abr', nota: 7},
+        { nome: 'Mai', nota: 7},
+        { nome: 'Jun', nota: 7},
+        ];
 
     return (
         <>
-            <div className="row">
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={dadosNotas}>
-                    <XAxis dataKey='mes' />
-                    <Line type='monotone'
-                        dataKey='nome' />
-                    <CartesianGrid stroke="red" strokeDasharray={"5"} />
-                <YAxis stroke="red"/>
-                <Bar  dataKey={'vendas'}/>
-                </LineChart>
-
-            </div>
-
+        <LineChart
+        width={500}
+        height={300}
+        data={dadosNotas}>
+        <XAxis dataKey='nome' />
+        <Line type='monotone'
+        dataKey='nota' />
+        </LineChart>
         </>
     );
-}
-export default GraficoLiha;
+  }
+export default GraficoLinha;
