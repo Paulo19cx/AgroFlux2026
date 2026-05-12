109f72904adee78e8deb7e6d16a2ff94d8753e65
Henrique Guimarães <henriqueguimaraes957@gmail.com>
2026-05-12 15:33:07 -0300

    Feat: Adicionando os graficos da pagina de relatorios.

--- ProgramaçãoAgroFlux/Front_End/src/components/template/GraficoLinha.jsx
+++ ProgramaçãoAgroFlux/Front_End/src/components/template/GraficoLinha.jsx
@@ -1,30 +1,61 @@
-import { LineChart, Pie, XAxis, PieChart, YAxis, Line, CartesianGrid, Bar } from "recharts";
+import { LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line } from 'recharts';
+import { RechartsDevtools } from '@recharts/devtools';
 
+// #region Sample data
+const data = [
+  {
+    name: 'Jan',
+    Lucro: 16500,
+    amt: 2400,
+  },
+  {
+    name: 'Fev',
+    Lucro: 20000,
+    amt: 2210,
+  },
+  {
+    name: 'Mar',
+    Lucro: 17500,
+    amt: 2290,
+  },
+  {
+    name: 'Abr',
+    Lucro: 22550,
+    amt: 2000,
+  },
+  {
+    name: 'Mai',
+    Lucro: 22400,
+    amt: 2181,
+  },
+  {
+    name: 'Jun',
+    Lucro: 30000,
+    amt: 2500,
+  },
+];
 
+// #endregion
+const GraficoLinha = ({ isAnimationActive = true }) => (
+  <LineChart
+    style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+    responsive
+    data={data}
+    margin={{
+      top: 5,
+      right: 30,
+      left: 20,
+      bottom: 5,
+    }}
+  >
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="name" />
+    <YAxis width="auto" />
+    <Tooltip />
+    <Legend />
+    <Line type="monotone" dataKey="Lucro" stroke="#82ca9d" isAnimationActive={isAnimationActive} />
+    <RechartsDevtools />
+  </LineChart>
+);
 
-
-function GraficoLinha() {
-
-    const dadosNotas = [
-        { nome: 'Jan', nota: 8},
-        { nome: 'Fev', nota: 6},
-        { nome: 'Mar', nota: 9},
-        { nome: 'Abr', nota: 7},
-        { nome: 'Mai', nota: 7},
-        { nome: 'Jun', nota: 7},
-        ];
-
-    return (
-        <>
-        <LineChart
-        width={500}
-        height={300}
-        data={dadosNotas}>
-        <XAxis dataKey='nome' />
-        <Line type='monotone'
-        dataKey='nota' />
-        </LineChart>
-        </>
-    );
-  }
 export default GraficoLinha;
